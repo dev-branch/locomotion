6b03715a85dfb45e3088c61699bf3cb0e0f4f180
Chyld Medford <chyld.medford@gmail.com>
2017-08-22 22:21:41 -0700

    index

--- spec/controllers/cars_controller_spec.rb
+++ spec/controllers/cars_controller_spec.rb
@@ -6,7 +6,13 @@ RSpec.describe CarsController, type: :controller do
   describe "GET /cars" do
     it "displays car index page" do
       get :index
-      assigns(:cars)
+      expect(assigns(:cars).size).to eql(3)
+      expect(response.status).to eq(200)
+      expect(response).to render_template("index")
+      expect(response.body).to include('ToyotaCamry2015')
+      expect(response.body).to include('<a href="/cars/new">New</a>')
+      id = assigns(:cars).first.id
+      expect(response.body).to include("<a href=\"/cars/#{id}\">#{id}</a>")
     end
   end
 end
