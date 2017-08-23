c91757aad82ae0c5c002cb5ac287bfd1facf5852
Chyld Medford <chyld.medford@gmail.com>
2017-08-22 23:37:35 -0700

    oops i did it again

--- spec/features/cars_spec.rb
+++ spec/features/cars_spec.rb
@@ -1,4 +1,9 @@
 require 'rails_helper'
 
 RSpec.feature "Cars", type: :feature do
+  scenario "User views /cars page" do
+    visit "/cars"
+    expect(page).to have_text("Cars")
+    save_and_open_page
+  end
 end
