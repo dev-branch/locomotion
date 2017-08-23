a8be4a3bba8ad4aa09a2e83be6fbb1012f90ff5b
Chyld Medford <chyld.medford@gmail.com>
2017-08-22 23:29:19 -0700

    edit & update

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -12,4 +12,30 @@ class CarsController < ApplicationController
   def show
     @car = Car.find(params[:id])
   end
+
+  def new
+    @car = Car.new
+  end
+
+  def create
+    @car = Car.new(params[:car].permit(:make, :model, :year, :vin))
+    if @car.save
+      redirect_to @car
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @car = Car.find(params[:id])
+  end
+
+  def update
+    @car = Car.find(params[:id])
+    if @car.update(params[:car].permit(:make, :model, :year, :vin))
+      redirect_to @car
+    else
+      render 'edit'
+    end
+  end
 end
--- spec/controllers/cars_controller_spec.rb
+++ spec/controllers/cars_controller_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe CarsController, type: :controller do
     end
   end
 
-  describe "DELETE /car/:id" do
+  describe "DELETE /cars/:id" do
     it "deletes a car" do
       car = Car.first
       delete :destroy, params: {id: car.id}
@@ -27,7 +27,7 @@ RSpec.describe CarsController, type: :controller do
     end
   end
 
-  describe "GET /car/:id" do
+  describe "GET /cars/:id" do
     it "displays a car" do
       car = Car.first
       get :show, params: {id: car.id}
@@ -36,6 +36,61 @@ RSpec.describe CarsController, type: :controller do
       expect(response.body).to include(car.vin)
       expect(response.body).to include("<a href=\"/cars/#{car.id}/edit\">edit</a>")
       expect(response.body).to include("<a rel=\"nofollow\" data-method=\"delete\" href=\"/cars/#{car.id}\">delete</a>")
+    end
+  end
+
+  describe "GET /cars/new" do
+    it "displays new car form" do
+      get :new
+      expect(response.status).to eq(200)
+      car = assigns(:car)
+      expect(car.is_a?(Car)).to be true
+    end
+  end
+
+  describe "POST /cars" do
+    it "creates a car" do
+      post :create, params: {car: {make: 'Tesla', model: '3', year: '2018', vin: '0123456789ABCDEFG'}}
+      car = assigns(:car)
+      expect(car.id).to_not be_nil
+      expect(response.status).to eq(302)
+      expect(response).to redirect_to car
+    end
+
+    it "does not creates a car - missing vin" do
+      get :create, params: {car: {make: 'Tesla', model: '3', year: '2018'}}
+      car = assigns(:car)
+      expect(car.id).to be_nil
+      expect(response.status).to eq(200)
+      expect(response).to render_template("new")
+      expect(response.body).to include("<li>Vin can&#39;t be blank</li>")
+    end
+  end
+
+  describe "GET /cars/:id/edit" do
+    it "displays edit form" do
+      car = Car.first
+      get :edit, params: {id: car.id}
+      expect(response.status).to eq(200)
+    end
+  end
+
+  describe "PUT /cars/:id" do
+    it "updates a car" do
+      car1 = Car.first
+      put :update, params: {id: car1.id, car: {make: 'Tesla', model: '3', year: '2018', vin: '0123456789ABCDEFG'}}
+      car2 = assigns(:car)
+      expect(response.status).to eq(302)
+      expect(response).to redirect_to car2
+    end
+
+    it "does not update a car - no vin" do
+      car1 = Car.first
+      put :update, params: {id: car1.id, car: {make: 'Tesla', model: '3', year: '2018', vin: ''}}
+      car2 = assigns(:car)
+      expect(response.status).to eq(200)
+      expect(response).to render_template("edit")
+      expect(response.body).to include("<li>Vin can&#39;t be blank</li>")
     end
   end
 end
