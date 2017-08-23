0e58909c8e550f3e6bdbcc6f5ba5d31ac5045e99
Chyld Medford <chyld.medford@gmail.com>
2017-08-22 22:40:59 -0700

    show

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -2,4 +2,14 @@ class CarsController < ApplicationController
   def index
     @cars = Car.all
   end
+
+  def destroy
+    car = Car.find(params[:id])
+    car.destroy
+    redirect_to cars_path
+  end
+
+  def show
+    @car = Car.find(params[:id])
+  end
 end
--- spec/controllers/cars_controller_spec.rb
+++ spec/controllers/cars_controller_spec.rb
@@ -15,4 +15,27 @@ RSpec.describe CarsController, type: :controller do
       expect(response.body).to include("<a href=\"/cars/#{id}\">#{id}</a>")
     end
   end
+
+  describe "DELETE /car/:id" do
+    it "deletes a car" do
+      car = Car.first
+      delete :destroy, params: {id: car.id}
+      expect(response.status).to eq(302)
+      expect(response).to redirect_to '/cars'
+      cars = Car.where(id: car.id)
+      expect(cars.size).to eql(0)
+    end
+  end
+
+  describe "GET /car/:id" do
+    it "displays a car" do
+      car = Car.first
+      get :show, params: {id: car.id}
+      expect(response.status).to eq(200)
+      expect(assigns(:car).id).to eql(car.id)
+      expect(response.body).to include(car.vin)
+      expect(response.body).to include("<a href=\"/cars/#{car.id}/edit\">edit</a>")
+      expect(response.body).to include("<a rel=\"nofollow\" data-method=\"delete\" href=\"/cars/#{car.id}\">delete</a>")
+    end
+  end
 end
